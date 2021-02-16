98b4fc37a605e395c5925fab959845817af70f2c
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-02-16 09:54:28 +0100

    admin dashboard

--- bucci-blog/routes/web.php
+++ bucci-blog/routes/web.php
@@ -2,21 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 
 
 Route::get('/', [App\Http\Controllers\PagesController::class, 'home'])->name('pages.home');
- 
+
 Route::get('/about', [App\Http\Controllers\PagesController::class, 'about'])->name('pages.about');
 Route::get('/contact', [App\Http\Controllers\PagesController::class, 'contact'])->name('pages.contact');
 Route::get('/service', [App\Http\Controllers\PagesController::class, 'service'])->name('pages.service');
+
+Route::get('/admin/index', [App\Http\Controllers\adminController::class, 'index'])->name('admin.index');
+
+
+
